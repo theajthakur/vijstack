721efe0f776d67c6c98da6655923402ee6c1e4b6
Vijay Singh <thakurvijayofficial@gmail.com>
2025-08-19 13:36:05 +0530

    Refactor: Structure change

---
+++ src/components/scroller/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FeatureProvided from "./_components/feature/page";
+
+export default function HorizontalScroller() {
+  return (
+    <div className="horizontal-scroller">
+      <FeatureProvided />
+    </div>
+  );
+}
