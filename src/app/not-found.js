34f43e6e8efc12b392a4808df3a13e1dada7c2b8
VIJAY THAKUR <vijaysingh.handler@gmail.com>
2025-10-05 20:20:47 +0530

    Refactor: FourZeroFour

---
+++ src/app/not-found.js
@@ -0,0 +1,30 @@
+import { Car, Rocket } from "lucide-react";
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="w-full min-h-[70vh] justify-center flex items-center mt-10">
+      <div className="px-6 py-8 rounded-xl max-w-md bg-black shadow-2xs text-secondary">
+        <div className="flex justify-center w-100">
+          <div className="p-3 border-secondary hover:border-accent hover:text-accent transition-all hover:scale-110 border-2 rounded-2xl">
+            <Rocket className="w-8 h-8" />
+          </div>
+        </div>
+        <div className="text-center my-3">
+          <h2 className="my-3">
+            We are prioritising our clients, We'll be fixing this as soon as we
+            get free!
+          </h2>
+          <button className="px-3 py-2 mt-3 border-white rounded-xl border-2 text-white transition-all cursor-pointer hover:bg-secondary hover:border-secondary">
+            <a href="/">
+              <div className="flex gap-2">
+                <Car />
+                <p> Go Back to Home</p>
+              </div>
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
